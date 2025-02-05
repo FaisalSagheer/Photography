b46c769fe86e66cc6fa6440187613cdc517abc7f
Faisal <faisalsagheerqureshi@gmail.com>
2025-02-05 21:11:34 +0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,37 @@
-import './App.css';
-import React from 'react';
+import './App.css'
+import React from 'react'
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Portfolio from './components/PortfolioPage/portfolio';
 import Contact from './components/ContactPage/Contact';
 import Pricing from './components/Pricing';
-import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
-
-const Layout = () => (
-  <>
-    <Navbar />
-    <Outlet/> {/* This renders the matched child route */}
-  </>
-);
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Layout />,
-      children: [
-        { path: "/", element: <Hero /> },
-        { path: "/portfolio", element: <Portfolio /> },
-        { path: "/pricing", element: <Pricing /> },
-        { path: "/contact", element: <Contact /> }
-      ]
+      element: <Hero />
+    },
+    {
+      path: "/portfolio",
+      element: <Portfolio />
+    },
+    {
+      path: "/pricing",
+      element: <Pricing />
+    },
+    {
+      path: "/contact",
+      element: <Contact />
     }
-  ]);
-
-  return <RouterProvider router={router} />;
+  ])
+  return (
+    <>
+      <RouterProvider router={router} />
+      <Navbar />
+    </>
+  )
 }
 
-export default App;
+export default App
