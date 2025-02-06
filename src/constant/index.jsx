94e69607e33ac192a508e4dd241de1be6b7d27db
Faisal <faisalsagheerqureshi@gmail.com>
2025-02-06 17:08:04 +0500

    changes

--- src/constant/index.jsx
+++ src/constant/index.jsx
@@ -1,7 +1,7 @@
 
 
 export const navigation = [
-  { name: 'Home', href: '/' },
+  { name: 'Home', href: '#/' },
   { name: 'Portfolio', href: '#/portfolio' },
   { name: 'Pricing', href: '#/pricing' },
   { name: 'Contact', href: '#/contact' }
