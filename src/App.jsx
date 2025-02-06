8d6c6e7c1a7f968caf2aeb63d90400ab6d769145
Faisal <faisalsagheerqureshi@gmail.com>
2025-02-06 16:32:34 +0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,26 @@
-import './App.css'
-import React from 'react'
+import './App.css';
+import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Portfolio from './components/PortfolioPage/portfolio';
 import Contact from './components/ContactPage/Contact';
 import Pricing from './components/Pricing';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
-  
   return (
     <>
-      <BrowserRouter basename='/Photography/'>
-      <Navbar />
-      <Routes>
-      <Route path='/' element={<Hero />}/>
-      <Route path='/portfolio' element={<Portfolio />}/>
-      <Route path='/pricing' element={<Pricing />}/>
-      <Route path='/contact' element={<Contact />}/>
-
-      </Routes>
-      </BrowserRouter>
-
+      <HashRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Hero />} />
+          <Route path='/portfolio' element={<Portfolio />} />
+          <Route path='/pricing' element={<Pricing />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+      </HashRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
